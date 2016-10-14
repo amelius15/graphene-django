c7300b13ce740a12b9200a21eb63b29d0a7569e1
Morgante Pell <morgante.pell@morgante.net>
2016-10-14 02:47:10 -0400

    export DjangoMutation

--- graphene_django/__init__.py
+++ graphene_django/__init__.py
@@ -1,9 +1,11 @@
 from .types import (
     DjangoObjectType,
+    DjangoMutation
 )
 from .fields import (
     DjangoConnectionField,
 )
 
 __all__ = ['DjangoObjectType',
-           'DjangoConnectionField']
+           'DjangoConnectionField',
+           'DjangoMutation']
